7056381858a410853e417fcafcf4427d46be6561
Sam Brougham <sam.brougham@gmail.com>
2023-02-18 22:09:16 -0500

    Use same technique for both parts

--- 2020/18/main.py
+++ 2020/18/main.py
@@ -1,6 +1,5 @@
 from time import time
-from typing import List, Tuple
-from re import match, findall
+from re import search, findall
 
 start = time()
 
@@ -8,37 +7,14 @@ with open("input.txt", encoding="utf8") as file:
     expressions = file.read().splitlines()
 
 
-def operate(operand_a: int, operand_b: int, operator: str) -> int:
-    if operator == "+":
-        return operand_a + operand_b
-    if operator == "*":
-        return operand_a * operand_b
+def evaluate_leftmost_operation(expression: str) -> str:
+    leftmost = search(r"^\d+ [\+|\*] \d+", expression)
 
+    while leftmost is not None:
+        expression = expression.replace(leftmost.group(), str(eval(leftmost.group())), 1)
+        leftmost = search(r"\d+ [\+|\*] \d+", expression)
 
-def evaluate_left_right(expression: List[str], index: int = 0) -> Tuple[int, int]:
-    accumulator, active_operation = 0, "+"
-
-    while index < len(expression):
-        if expression[index] == "(":
-            result, index = evaluate_left_right(expression, index + 1)
-            accumulator = operate(accumulator, result, active_operation)
-
-        if expression[index] == ")":
-            return accumulator, min(index + 1, len(expression) - 1)
-
-        if expression[index] in ["+", "*"]:
-            active_operation = expression[index]
-
-        if match(r"\d", expression[index]):
-            curr_num = expression[index]
-            while index + 1 < len(expression) and match(r"\d", expression[index + 1]):
-                index += 1
-                curr_num += expression[index]
-            accumulator = operate(accumulator, int(curr_num), active_operation)
-
-        index += 1
-
-    return accumulator, index
+    return expression
 
 
 def evaluate_addition(expression: str) -> str:
@@ -60,31 +36,31 @@ def evaluate_multiplication(expression: str) -> str:
     return expression
 
 
-def evaluate_expression_without_subexpression(expression: str):
-    while match(r"^\d+$", expression) is None:
+def evaluate_addition_first(expression: str) -> str:
+    while search(r"^\d+$", expression) is None:
         expression = evaluate_addition(expression)
         expression = evaluate_multiplication(expression)
 
     return expression
 
 
-def evaluate_addition_first(expression: str):
+def evaluate(expression: str, evaluation_function) -> str:
     subexpressions = findall(r"\([^\(]+?\)", expression)
 
     while subexpressions is not None and len(subexpressions) > 0:
         for subexpression in subexpressions:
-            evaluated = evaluate_expression_without_subexpression(subexpression[1:-1])
+            evaluated = evaluation_function(subexpression[1:-1])
             expression = expression.replace(subexpression, evaluated)
 
         subexpressions = findall(r"\([^\(]+?\)", expression)
 
-    return int(evaluate_expression_without_subexpression(expression))
+    return int(evaluation_function(expression))
 
 
 left_right_expression_sum, addition_first_expression_sum = 0, 0
 for expression in expressions:
-    left_right_expression_sum += evaluate_left_right([*expression])[0]
-    addition_first_expression_sum += evaluate_addition_first(expression)
+    left_right_expression_sum += evaluate(expression, evaluate_leftmost_operation)
+    addition_first_expression_sum += evaluate(expression, evaluate_addition_first)
 
 time_elapsed = round(time() - start, 5)
 
