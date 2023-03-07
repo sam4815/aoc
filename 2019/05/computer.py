d735446ee0451a5f4150937672cefd83565bfb74
sam <sam.brougham@gmail.com>
2023-03-07 08:06:40 -0500

    Add relative mode to computer

--- 2019/05/computer.py
+++ 2019/05/computer.py
@@ -3,10 +3,9 @@ from typing import List
 
 class Computer:
     def __init__(self, memory, inputs):
-        self.memory = memory[:]
+        self.memory = memory[:] + [0] * 4096
         self.inputs = inputs
-        self.output = 0
-        self.pc = 0
+        self.output, self.pc, self.base = 0, 0, 0
         self.terminated = False
 
     def current_instruction(self):
@@ -17,56 +16,76 @@ class Computer:
 
     def params(self, num) -> List[int]:
         params = []
+
         for i in range(1, num + 1):
             param = self.memory[self.pc + i]
             mode = self.current_instruction_str()[-2 - i]
-            params.append(self.memory[param] if mode == "0" else param)
+
+            if mode == "0":
+                params.append(self.memory[param])
+            if mode == "1":
+                params.append(param)
+            if mode == "2":
+                params.append(self.memory[param + self.base])
+
         return params
+
+    def memory_address(self, offset):
+        address = self.memory[self.pc + offset]
+        mode = self.current_instruction_str()[-2 - offset]
+
+        if mode == "2":
+            return address + self.base
+
+        return address
 
     def set(self, position, value):
         self.memory[position] = value
 
     def add(self):
         a, b = self.params(2)
-        self.set(self.memory[self.pc + 3], a + b)
+        self.set(self.memory_address(3), a + b)
         self.pc += 4
 
     def mul(self):
         a, b = self.params(2)
-        self.set(self.memory[self.pc + 3], a * b)
+        self.set(self.memory_address(3), a * b)
         self.pc += 4
 
     def set_input(self):
-        self.set(self.memory[self.pc + 1], self.inputs[0])
-        self.inputs = self.inputs[1:]
+        self.set(self.memory_address(1), self.inputs.pop(0))
         self.pc += 2
 
     def set_output(self):
         self.output = self.params(1)[0]
         self.pc += 2
 
+    def set_base(self):
+        self.base += self.params(1)[0]
+        self.pc += 2
+
     def jump_if_true(self):
-        a, pointer = self.params(2)
+        a, address = self.params(2)
         if a != 0:
-            self.pc = pointer
+            self.pc = address
         else:
             self.pc += 3
 
     def jump_if_false(self):
-        a, pointer = self.params(2)
+        a, address = self.params(2)
         if a == 0:
-            self.pc = pointer
+            self.pc = address
         else:
             self.pc += 3
 
     def less_than(self):
         a, b = self.params(2)
-        self.set(self.memory[self.pc + 3], 1 if a < b else 0)
+        self.set(self.memory_address(3), 1 if a < b else 0)
         self.pc += 4
 
     def equals(self):
         a, b = self.params(2)
-        self.set(self.memory[self.pc + 3], 1 if a == b else 0)
+        self.set(self.memory_address(3), 1 if a == b else 0)
         self.pc += 4
 
     def run(self):
@@ -90,6 +109,8 @@ class Computer:
                 self.less_than()
             elif opcode == 8:
                 self.equals()
+            elif opcode == 9:
+                self.set_base()
 
         self.terminated = True
         return self.output
