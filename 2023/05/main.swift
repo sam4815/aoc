439215669c8bd9807eb896d455870354fcdd6617
sam <sam.brougham@gmail.com>
2023-12-05 10:05:34 -0500

    Switch to flatMap

--- 2023/05/main.swift
+++ 2023/05/main.swift
@@ -12,47 +12,35 @@ let maps = contents[1...].map {
 }
 
 func mapRanges(ranges: [(Int, Int)], rules: [[Int]]) -> [(Int, Int)] {
-  var currRanges: [(Int, Int)] = ranges
-  var nextRanges: [(Int, Int)] = []
-
-  outer: while currRanges.count > 0 {
-    let (min, max) = currRanges.removeFirst()
-
+  ranges.flatMap { (min, max) in
     for rule in rules {
       // Rule bounds the range
       if min >= rule[1] && max < (rule[1] + rule[2]) {
-        nextRanges.append((rule[0] + (min - rule[1]), rule[0] + (max - rule[1])))
-        continue outer
+        return [(rule[0] + (min - rule[1]), rule[0] + (max - rule[1]))]
       }
       // Range bounds the rule
       if min < rule[1] && max >= (rule[1] + rule[2]) {
-        currRanges.append((min, rule[1] - 1))
-        currRanges.append((rule[1] + rule[2], max))
-
-        nextRanges.append((rule[0], rule[0] + rule[2]))
-        continue outer
+        return mapRanges(ranges: [(min, rule[1] - 1)], rules: rules)
+          + mapRanges(ranges: [(rule[1] + rule[2], max)], rules: rules) + [
+            (rule[0], rule[0] + rule[2])
+          ]
       }
       // Range partially intersects left side of rule
       if min < rule[1] && max >= rule[1] && max < (rule[1] + rule[2]) {
-        currRanges.append((min, rule[1] - 1))
-
-        nextRanges.append((rule[0], rule[0] + (max - rule[1])))
-        continue outer
+        return mapRanges(ranges: [(min, rule[1] - 1)], rules: rules) + [
+          (rule[0], rule[0] + (max - rule[1]))
+        ]
       }
       // Range partially intersects right side of rule
       if min > rule[1] && min < (rule[1] + rule[2]) && max >= (rule[1] + rule[2]) {
-        currRanges.append((rule[1] + rule[2], max))
-
-        nextRanges.append((rule[0] + (min - rule[1]), rule[0] + rule[2]))
-        continue outer
+        return mapRanges(ranges: [(rule[1] + rule[2], max)], rules: rules) + [
+          (rule[0] + (min - rule[1]), rule[0] + rule[2])
+        ]
       }
     }
-
     // Range does not intersect rule at all
-    nextRanges.append((min, max))
+    return [(min, max)]
   }
-
-  return nextRanges
 }
 
 let partialSeeds: [(Int, Int)] = stride(from: 0, to: seeds.count, by: 1).map {
