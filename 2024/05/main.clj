ec17e8dbe3e1eda605c4b4d534b495fa5aed9802
sam <sam.brougham@gmail.com>
2024-12-06 07:05:48 -0500

    Prefer map/keep-indexed

--- 2024/05/main.clj
+++ 2024/05/main.clj
@@ -11,6 +11,7 @@
                 (partition 2)))
 
 (def after (reduce (fn [a [b c]] (assoc a b (conj (get a b []) c))) {} rules))
+
 (def before (reduce (fn [a [b c]] (assoc a c (conj (get a c []) b))) {} rules))
 
 (def updates (as-> (str/split (slurp "input.txt") #"\n\n") v
@@ -19,28 +20,24 @@
                (map #(mapv parse-long (re-seq #"\d+" %)) v)))
 
 (defn is-ordered [pages-update]
-  (loop [i 1]
-    (if (= i (count pages-update))
-      true
-      (if (and
-            (empty? (set/intersection (set (get after (nth pages-update i) [])) (set (subvec pages-update 0 i))))
-            (empty? (set/intersection (set (get before (nth pages-update i) [])) (set (subvec pages-update (inc i))))))
-        (recur (inc i))
-        false))))
+  (every? true? (map-indexed
+                  (fn [i page]
+                    (and (empty? (set/intersection (set (get after page [])) (set (subvec pages-update 0 i))))
+                         (empty? (set/intersection (set (get before page [])) (set (subvec pages-update (inc i)))))))
+                  pages-update)))
 
-(defn sum-middle [pages-update]
-  (->> (map-indexed (fn [i page]
-                      (let [removed (concat (subvec pages-update 0 i) (subvec pages-update (inc i)))
-                            a (set/intersection (set (get after page [])) (set removed))
-                            b (set/intersection (set (get before page [])) (set removed))]
-                        (if (= (count a) (count b)) page nil))) pages-update)
-       (filter some?)
-       (reduce +)))
-
+(defn find-middle [pages-update]
+  (first (keep-indexed
+           (fn [i page]
+             (let [removed (concat (subvec pages-update 0 i) (subvec pages-update (inc i)))
+                   a (set/intersection (set (get after page [])) (set removed))
+                   b (set/intersection (set (get before page [])) (set removed))]
+               (when (= (count a) (count b)) page)))
+           pages-update)))
 
 (def grouped (group-by is-ordered updates))
 
-(println (format "The sum of the correctly-ordered middle pages is %d." (reduce + (map sum-middle (get grouped true)))))
-(println (format "The sum of the incorrectly-ordered middle pages is %d." (reduce + (map sum-middle (get grouped false)))))
+(println (format "The sum of the correctly-ordered middle pages is %d." (reduce + (map find-middle (get grouped true)))))
+(println (format "The sum of the incorrectly-ordered middle pages is %d." (reduce + (map find-middle (get grouped false)))))
 (println (format "Solution generated in %.3fs." (float (/ (- (System/currentTimeMillis) start-time) 1000))))
 
